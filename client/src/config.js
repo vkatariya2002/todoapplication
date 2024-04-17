f028c996548016f94964d0bb42c9b1d26d8d7b9f
vishal katariya <katariyavishal2002@gmail.com>
2024-04-17 23:04:51 +0530

    hosting

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,2 @@
-export const API_URL = 'http://localhost:8000';
-// export const API_URL = 'https://vishaltodoapplication.onrender.com'
+// export const API_URL = 'http://localhost:8000';
+export const API_URL = 'https://todo-app-server-vishal.cyclic.app'
